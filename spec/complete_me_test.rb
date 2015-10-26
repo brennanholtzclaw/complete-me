0fcf81b2f1bcbbb4ff190db69f977433127cc304
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-10-26 16:29:33 -0600

    Initial Commit

---
+++ spec/complete_me_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/complete_me'
+
+class CompleteMeTest < MiniTest::Test
+
+def test_it_exists
+  assert CompleteMe.new
+end
+
+
+end
